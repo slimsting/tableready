a76655537b50ab145e44d4ce8645fdff27c3383d
slimsting <newbon.gadson@.com>
2024-08-05 00:35:39 +0300

    change url to point to deployed backend

--- frontend/src/Context/StoreContext.jsx
+++ frontend/src/Context/StoreContext.jsx
@@ -6,8 +6,8 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "http://localhost:4000";
-  // const url = "https://tableready.onrender.com";
+  // const url = "http://localhost:4000";
+  const url = "https://tableready.onrender.com";
   const [token, setToken] = useState("");
   const [food_list, setFoodList] = useState([]);
 
